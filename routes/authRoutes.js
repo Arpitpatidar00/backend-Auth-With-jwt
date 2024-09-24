187284b53e00c457e383d2c858206352244fd73e
Arpit Patidar <123389729+Arpitpatidar00@users.noreply.github.com>
2024-09-24 14:23:55 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-
+router.get('/protected', authMiddleware, (req, res) => {
+  res.status(200).json({ message: 'You have accessed a protected route', user: req.user });
+});
 module.exports = router;
