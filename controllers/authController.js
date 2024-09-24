50f4a23bfedbed7a18cc4a3a4f08c0f965eb60f6
Arpit Patidar <arpitpatidar@Arpits-MacBook-Air.local>
2024-09-24 14:14:17 +0530

    Auth with jwt

---
+++ controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Signup controller
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: 'User already exists' });
+  }
+};
+
+// Login controller
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
